31fd9f10298b03da01fe0cc4170ae1aea0092bb0
Marion <marionbailleux07@gmail.com>
2023-11-15 11:49:48 +0100

    add https

--- components/Home.js
+++ components/Home.js
@@ -11,7 +11,7 @@ function Home() {
   const [moviesData, setMoviesData] = useState([]);
 
   useEffect(() => {
-    fetch("mymoviz-backend-nine-ruddy.vercel.app/movies")
+    fetch("https://mymoviz-backend-nine-ruddy.vercel.app/movies")
       .then((response) => response.json())
       .then((data) => {
         setMoviesData(data.results);
