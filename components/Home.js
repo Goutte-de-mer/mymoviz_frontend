3bd35a959624d2a453137271b8e4143e94987711
Marion <marionbailleux07@gmail.com>
2023-11-15 11:43:32 +0100

    change fetch url

--- components/Home.js
+++ components/Home.js
@@ -11,7 +11,7 @@ function Home() {
   const [moviesData, setMoviesData] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/movies")
+    fetch("mymoviz-backend-nine-ruddy.vercel.app/movies")
       .then((response) => response.json())
       .then((data) => {
         setMoviesData(data.results);
